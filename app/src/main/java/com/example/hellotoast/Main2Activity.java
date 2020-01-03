e31c543c2f49f4b3210b71e43f4acb4b5646dccb
azeedia <azeedia@starmicronics.com>
2020-01-03 11:17:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+
+    }
+
+    public void back(View view) {
+        finish();
+    }
+}
